846013f21dc7778be5d1d41e3b608fd796a4cc0b
zimkus <mutka01@gmail.com>
2024-04-09 20:33:50 +0300

    pirmas uzd

--- pirma.js
+++ pirma.js
@@ -1,8 +1,19 @@
-let temp = [1, 2, 7, 3, 4, 6, -1, 2, 4, -3, -3, -5, 1, 5, 6, 7, 5, 9, 2, 3, 6, 7, 8, 6, 3, 6, 7, 5, 6, 7, 1];
-let min = 0;
-let max = 0;
-temp.forEach(() => {
-    min = Math.min(...temp);
-    max = Math.max(...temp);
+const fs = require("fs");
+const data = fs.readFileSync(process.argv[2]).toString().split(" ");
+
+let min = data[0];
+let max = data[0];
+let minD = 1;
+let maxD = 1;
+
+data.forEach((d, i) => {
+    if (d < min) {
+        min = d;
+        minD = i + 1;
+    }
+    if (d > max) {
+        max = d;
+        maxD = i + 1;
+    }
 });
-console.log(min, max);
+console.log(`Min: ${min}, ${minD} diena Max: ${max}, ${maxD} diena`);
